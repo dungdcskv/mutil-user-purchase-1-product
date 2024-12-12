55f600d1fc88078256e53bf214596fc294a86bb0
dungnv <dungnv@meeyland.com>
2024-12-12 17:54:24 +0700

    second

--- redis.v1.js
+++ redis.v1.js
@@ -15,7 +15,6 @@ app.use(express.json());
 app.get('/order', async (req, res) => {
 
     try {
-        console.log('111');
         const date = new Date().getTime()
 
         // gia su so luong ton khi hien tai la 10
@@ -26,20 +25,16 @@ app.get('/order', async (req, res) => {
 
         // gia su moi lan mua hang chi mua 1 sp
         const slMua = 1
-        console.log('222');
         // so luong ban ra, neu chua ban thi set = 0, con neu ba thi update + 1 moi lan user order thanh cong
         const getKey = await exists(keyName)
-        console.log('333', getKey);
         if (!getKey) {
-            console.log('444');
             // set = 0
             await set(keyName, 0)
         }
-        console.log('555');
         // lay so luong ban ra
-        const slBanRa = await get(keyName);
+        let slBanRa = parseInt(await get(keyName));
 
-        console.log(`Truoc khi user order thanh cong thi sl ban ra ===`, slBanRa);
+        console.log(`Truoc khi user order thanh cong thi sl ban ra ===`, slBanRa, slMua, slBanRa + slMua);
 
         // neu so luong ban ra + so luong mua > sl ton kho return false
         if (slBanRa + slMua > slTonKho) {
@@ -50,7 +45,7 @@ app.get('/order', async (req, res) => {
                 time: date
             })
         }
-        console.log('5555');
+
         // neu user order thanh cong
         slBanRa = await incrby(keyName, slMua)
 
@@ -64,8 +59,7 @@ app.get('/order', async (req, res) => {
         })
 
     } catch (error) {
-        console.error('Error during order processing:', err);
-        return res.status(500).json({ status: 'Error', message: 'Internal Server Error' });
+        return res.status(500).json({ status: 'Error', message: error.message });
     }
 })
 
